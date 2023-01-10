7f16c65a971ec6a24e69ffbbabda089c919bdf9f
koniczek <sz.zalas@gmail.com>
2023-01-10 10:42:05 +0100

    Minor changes

--- main.py
+++ main.py
@@ -34,6 +34,27 @@ def modelModified(x,t,k0,k1,k2,k3,R_inv,B,l, m, J, d, g):
     x1p = x2
     x2p = (u - d * x2 - m * g * l * np.sin(x1)) / J
     return np.array([x1p, x2p[0]])
+
+def modelLinearInf(x, t, K, l, m, J, d, g,A,B):
+    x1 = x[0]
+    x2 = x[1]
+    X=np.array([[x1], [x2]])
+    X0=np.array([[np.pi], [0]])
+    u = -K @ (X - X0)
+    xx = A @ X + B * u
+    return np.array([xx[0,0],xx[1,0]])
+
+def modelLinearFin(x,t,k0,k1,k2,k3,R_inv,B,l, m, J, d, g,A):
+    x1 = x[0]
+    x2 = x[1]
+    Pt = np.array([[k0(t), k1(t)], [k2(t), k3(t)]])
+    Kt = R_inv @ B.T @ Pt
+    X = np.array([[x1], [x2]])
+    X0 = np.array([[np.pi], [0]])
+    u = -Kt @ (X - X0)
+    xx=A@X+B*u
+    return np.array([xx[0,0],xx[1,0]])
+
 def riccati(p,t,A,B,R,q):
     Pt=p.reshape((2,2))
     Pp=(Pt@A-Pt@B*R@B.T@Pt+A.T@Pt+q)
@@ -48,7 +69,7 @@ def zadanie1(active):
         J = 1
         d = 0.5
         g = 9.81
-        t = np.linspace(0, 5, 1001)
+        t = np.linspace(0, 10, 1001)
         #
         #base
         ans1 = odeint(model, y0=[np.pi / 4, 0], t=t, args=(u, l, m, J, d, g))
@@ -71,27 +92,38 @@ def zadanie1(active):
         R_inv = np.array([1])
         BT = np.transpose(B)
         K = R_inv @ BT @ P
+        print(K)
         ans2 = odeint(modelMod, y0=Y0, t=t, args=(K, l, m, J, d, g))
         plt.figure('With LQR - inf')
         plt.plot(t, ans2[:, 0]-np.pi, label='x1')
         plt.plot(t, ans2[:, 1], label='x2')
         plt.legend()
+        ansX = odeint(modelLinearInf, y0=Y0, t=t, args=(K, l, m, J, d, g,A,B))
+        plt.figure('With LQR StateSpace - inf')
+        plt.plot(t, ansX[:, 0]-(2*np.pi) , label='x1')
+        plt.plot(t, ansX[:, 1], label='x2')
+        plt.legend()
         #
         #finite
-        t = np.linspace(0, 1, 101)
+        t = np.linspace(0, 10, 101)
         t_inv = np.flip(t, 0)
         S = np.array([[1, 0], [0, 1]])
         P_t = odeint(riccati, y0=[1, 0, 0, 1], t=t, args=(A,B,r,q))
         P_t1 = np.array([[P_t[1, 0], P_t[-1, 1]], [P_t[1, 2], P_t[-1, 3]]])
-        print(P_t1)
         Pt_inter0 = scipy.interpolate.interp1d(t_inv, P_t[:, 0], fill_value='extrapolate')
         Pt_inter1 = scipy.interpolate.interp1d(t_inv, P_t[:, 1], fill_value='extrapolate')
         Pt_inter2 = scipy.interpolate.interp1d(t_inv, P_t[:, 2], fill_value='extrapolate')
         Pt_inter3 = scipy.interpolate.interp1d(t_inv, P_t[:, 3], fill_value='extrapolate')
+        t = np.linspace(0, 9, 101)
         SystemSym = odeint(modelModified, y0=Y0, t=t, args=(Pt_inter0, Pt_inter1, Pt_inter2, Pt_inter3,R_inv,B,l,m,J,d,g))
         plt.figure('With LQR - fin t=(0,1)')
         plt.plot(t, SystemSym[:, 0]-np.pi, label='x1')
         plt.plot(t, SystemSym[:, 1], label='x2')
+        plt.legend()
+        SystemSymFin = odeint(modelLinearFin, y0=Y0, t=t, args=(Pt_inter0, Pt_inter1, Pt_inter2, Pt_inter3,R_inv,B,l,m,J,d,g,A))
+        plt.figure('With LQR StateSpace - fin t=(0,1)')
+        plt.plot(t, SystemSymFin[:, 0]-(2*np.pi), label='x1')
+        plt.plot(t, SystemSymFin[:, 1], label='x2')
         plt.legend()
         #
         plt.show()
